12dfad0e0cff47a3bda5f94572a44ff29c3f6d74
dashixiong <2788347834@qq.com>
2021-06-08 22:12:19 +0800

    创建三个数据表

--- src/migration/1623075070940-CreatePosts.ts
+++ src/migration/1623075070940-CreatePosts.ts
@@ -1,20 +1,21 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import {MigrationInterface, QueryRunner,Table} from "typeorm";
 
-export class CreateUsers1623072625181 implements MigrationInterface {
+export class CreatePosts1623075070940 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-      return await  queryRunner.createTable(new Table({
-            name: 'users',
+       return await queryRunner.createTable(new Table({
+            name: 'posts',
             columns: [
                 {name: 'id', isGenerated: true, type: 'int', generationStrategy: 'increment',isPrimary:true},
                 {name: 'username',type: 'varchar'},
                 {name: 'password_digest',type: 'varchar'},
+                {name: 'author_id',type: 'int'},
             ]
         }))
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-       return await queryRunner.dropTable('users')
+       return await queryRunner.dropTable('posts')
     }
 
 }
