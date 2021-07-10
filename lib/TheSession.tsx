403656b4a9bf3efb1ea77da808aee2b1a7bec3a1
dashixiong <2788347834@qq.com>
2021-07-10 12:13:04 +0800

    添加本地环境变量

--- lib/TheSession.tsx
+++ lib/TheSession.tsx
@@ -4,7 +4,7 @@ import {withIronSession} from "next-iron-session";
 export const theSession = (handler: NextApiHandler) => {
     return withIronSession(handler, {
         // password: process.env.SECRET_COOKIE_PASSWORD,
-        password: 'c2a85490-cc60-4f21-94e8-8dc5dd3220da',
+        password: process.env.SECRET,
         cookieName: 'blog',
         cookieOptions: {secure: false}
     });
